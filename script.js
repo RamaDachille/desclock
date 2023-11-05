d1cde8281dc35b53d58087dbd9fbc03ea5d60322
Rama DAchille <dachille108@gmail.com>
2023-11-05 01:32:10 +0100

    added js stopwatch start functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('Setup JS');
+const button = document.querySelector(".button");
+let minutes = document.querySelector(".mins");
+let secsAndMills = document.querySelector(".blue-text");
+
+// STOPWATCH
+let timeCounter = 0;
+let stopwatchPaused = false
+
+let mins = 0;
+let secs = 0;
+let mills = 0;
+
+const startTimer = function () {
+  setInterval(() => {
+    mins = (Math.floor(timeCounter / 100 / 60) +'').padStart(2, '0')
+    secs = (Math.floor((timeCounter / 100) % 60) +'').padStart(2, '0')
+    mills = (timeCounter % 100 +'').padStart(2, '0')
+
+    minutes.textContent = mins
+    secsAndMills.textContent = `${secs}:${mills}`
+    
+    timeCounter++;
+  }, 10);
+};
+
+button.addEventListener("click", startTimer);
